952efd8a3a744d926866f4e178743ab24ccbda4b
seham <sehamsaad590@gmail.com>
2022-11-06 20:05:39 +0200

    portfolio with bootstrap & Html

---
+++ portfolio.js
@@ -0,0 +1,40 @@
+
+var myText = 'Front End Developer';
+console.log(myText[0]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// (function($){
+// "use strict"
+//     if ($('.typed-text.output').length==1){
+//         var type_strings=$('.type-text').text();
+//         var typed= new typed('.type-text.output ',{
+//             strings: typed_strigs.split(','),
+//             typeSpeed:100,
+//             backSpeed:20,
+//             smartBackspace:false,
+//             loop:true
+
+//         });
+//     }
+// })(jQuery);
